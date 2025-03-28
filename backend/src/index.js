01b51145a1fe4022dff154a94bdd25046a4613ca
Vijay Patel <vijaypatel3207@gmail.com>
2025-03-28 13:14:54 +0530

    backend project setup done

---
+++ backend/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { PORT, URI } = require('./config');
+
+
+
+const app = express();
+
+mongoose.connect(URI)
+.then(() => console.log("Db is connected successfully"))
+.catch((error) => console.log(error));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: '',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true
+}));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
