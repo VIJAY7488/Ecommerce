02a0eefeab742b17b359d9152eecf149ba0db421
Vijay Patel <vijaypatel3207@gmail.com>
2025-03-28 14:25:14 +0530

    frontend project detup is done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+    </div>
+  )
+}
+
+export default App
